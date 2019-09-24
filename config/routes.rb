85e3fc59e0566c26312d5b65a5b437dd470964e0
Patrick Dunn <Patrickdunn@patorikacBookea.lan>
2019-09-24 21:59:19 +1000

    Updated routes on home, about, and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :products
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 end
