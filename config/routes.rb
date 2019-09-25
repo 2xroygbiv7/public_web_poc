ae5b27acc0b84eb23675caae8de6750cad3eb5fa
Patrick Dunn <Patrickdunn@patorikacBookea.lan>
2019-09-25 23:13:28 +1000

    created custom route for product#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products, except: [:show]
+  get 'product/:id', to: 'products#show', as: 'product_show'
+  
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
