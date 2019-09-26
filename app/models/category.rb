83b2d937a2cbb68f5b0a96a4061ad13c971b1df8
Patrick Dunn <Patrickdunn@patorikacBookea.lan>
2019-09-27 00:15:31 +1000

    Implemented database relationship between products and category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   validates_presence_of :title, :sub_category
+  has_many :products
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,18 @@
-ActiveRecord::Schema.define(version: 2019_09_22_021227) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2019_09_26_135741) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
@@ -16,5 +29,9 @@ ActiveRecord::Schema.define(version: 2019_09_22_021227) do
     t.text "main_image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
+
+  add_foreign_key "products", "categories"
 end
