0d6b27a36f9d707b5557be9bea7eecaa5ab7e52b
Patrick Dunn <patrick@theonevehicle.com>
2019-09-29 01:52:21 +1000

    Implemented scopes for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
   validates :code, presence: true
   belongs_to :category
+  
+  def self.pens
+    pens = Category.where(parent: "pen").ids
+    where(id: pens)
+  end
+  scope :markers, -> { where(id: Category.where(parent: "marker").ids) }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 category_list = [
   [ "ballpoint", "pen" ],
   [ "energel", "pen"],
-  [ "permanent", "markers"],
-  [ "whiteboard", "markers"]
+  [ "permanent", "marker"],
+  [ "whiteboard", "marker"]
 ]
 
 category_list.each do |title, parent|
@@ -18,25 +18,25 @@ product_list = [
    [ "BL107", 
      "Energel X", 
      "0.7mm retractable gel pen\nQuick dry ink. Ideal for left-handed writers\nSuper smooth, high quality liquid gel ink",
-     "http://placehold.it/300x200",
+     "http://placehold.it/150x100",
      Category.find_by(title: "energel").id
    ],
    [ "BL110", 
      "Energel X", 
      "1.0mm retractable gel pen\nQuick dry ink. Ideal for left-handed writers\nSuper smooth, high quality liquid gel ink",
-     "http://placehold.it/300x200",
+     "http://placehold.it/150x100",
      Category.find_by(title: "energel").id
    ],
    [ "N50", 
      "Pentel Pen Permanent Marker", 
      "3.0mm to 4.0mm bullet point permanent marker\nRobust aluminium barrel. Ideal of touch work environments\nBonded fibre tip retains its shape",
-     "http://placehold.it/300x200",
+     "http://placehold.it/150x100",
      Category.find_by(title: "permanent").id
    ],
    [ "N60", 
      "Pentel Pen Permanent Marker", 
      "4.0mm to 5.0mm bullet point permanent marker\nRobust aluminium barrel. Ideal of touch work environments\nBonded fibre tip retains its shape",
-     "http://placehold.it/300x200",
+     "http://placehold.it/150x100",
      Category.find_by(title: "permanent").id
    ]
 ]
