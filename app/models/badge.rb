3c35620113b687b86e2b1bc3aa7f17f602f91bf0
Patrick Dunn <patrick@theonevehicle.com>
2019-09-29 14:28:46 +1000

    Added default to badge model for image

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,3 +1,9 @@
 class Badge < ApplicationRecord
   belongs_to :product
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.image ||= Placeholder.image_generator(height:50, width: 50)
+  end
 end
