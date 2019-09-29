3d0dcdd12b172672d755351c6e1aeb829c63b838
Patrick Dunn <patrick@theonevehicle.com>
2019-09-29 12:55:08 +1000

    Added image to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,11 @@
 class Category < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :parent
   has_many :products
+  
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.image ||= Placeholder.image_generator(height: 50, width: 200)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_28_063834) do
+ActiveRecord::Schema.define(version: 2019_09_29_024227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_09_28_063834) do
     t.string "parent"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "image"
   end
 
   create_table "products", force: :cascade do |t|
