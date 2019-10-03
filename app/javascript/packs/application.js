c41ca1c29a77269b295dab6351284328311ea028
Patrick Dunn <patrick@theonevehicle.com>
2019-10-03 22:28:37 +1000

    Implemented jQuery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,4 +20,4 @@ import './bootstrap_custom.js'
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-//console.log('Hello World from Webpacker');
+console.log('Hello World from Webpacker');
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,13 +2,20 @@ const { environment } = require('@rails/webpacker')
 
 
 const webpack = require('webpack')
-environment.plugins.append(
-  'Provide',
+environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
   })
 )
+// const webpack = require('webpack')
+// environment.plugins.append(
+//   'Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery',
+//     jQuery: 'jquery',
+//     Popper: ['popper.js', 'default']
+//   })
+// )
 
 module.exports = environment
