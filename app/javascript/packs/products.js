8260dc22d65729f610284f4904d43bef1f3e7769
Patrick Dunn <patrick@theonevehicle.com>
2019-10-15 19:51:28 +1100

    Implemented show page for products

--- app/javascript/packs/products.js
+++ app/javascript/packs/products.js
@@ -10,9 +10,9 @@ require("channels")
 
 require('jquery')
 
-import '../stylesheets/application'
-//import '../stylesheets/products'
-import './bootstrap_custom.js'
+//import '../stylesheets/application'
+import '../stylesheets/products'
+//import './bootstrap_custom.js'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -21,6 +21,5 @@ import './bootstrap_custom.js'
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker');
-require("packs/test.js")
+console.log('Hello World from products layout!');
 
