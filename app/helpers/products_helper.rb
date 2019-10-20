99d4ce45386c67e690e3e673f75c6fb9084f47af
Patrick Dunn <patrick@theonevehicle.com>
2019-10-20 22:21:05 +1100

    Uplaoded file upload button. and refactored placeholder functionality

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,14 @@
 module ProductsHelper
+  def image_generator(height:, width:)
+    "http://placehold.it/#{height}x#{width}"
+  end
+  def product_img img, type
+    if img.model.main_image?
+      img
+    elsif type == 'thumb'
+      image_generator(height: '350', width: '200')
+    else
+      image_generator(height: '300', width: '200')
+    end
+  end
 end
